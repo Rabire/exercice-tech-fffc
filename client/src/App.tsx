91241523f9849b849f4b5fb77ab616c5b295a133
Rabire <rabireh@outlook.fr>
2025-09-10 17:51:03 +0200

    chore(client): setup client with vite + tailwindcss & schadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;

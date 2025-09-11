460b4eb9e593a7df1b3cdc13895c4e3f5b91b9a4
Rabire <rabireh@outlook.fr>
2025-09-11 11:55:29 +0200

    feat(client): wip - generate data ui

--- client/src/components/stepper-content.tsx
+++ client/src/components/stepper-content.tsx
@@ -1,6 +1,7 @@
 import { useProgressStepper } from "@/hooks/progress-context";
 import UploadZone from "./file-upload";
 import ProgressStepper from "./progress-stepper";
+import { Button } from "./ui/button";
 
 const StepperContent = () => {
   const { activeStep } = useProgressStepper();
@@ -9,7 +10,21 @@ const StepperContent = () => {
     <>
       <ProgressStepper className="max-w-2xl" />
 
-      {activeStep === 0 && <UploadZone />}
+      {activeStep === 0 && (
+        <div className="max-w-2xl  space-y-6 w-full">
+          <UploadZone />
+
+          {/* TODO: plug the feature bellow */}
+          <div className="flex flex-col gap-2 items-center">
+            <p className="text-sm text-muted-foreground">
+              Pas de données à convertir ? Générez les à l'aide de l'IA
+            </p>
+            <Button size="sm" variant="outline">
+              Générer des données
+            </Button>
+          </div>
+        </div>
+      )}
       {/* {activeStep === 1 && <ConvertZone />} */}
       {/* {activeStep === 2 && <DownloadZone />} */}
     </>

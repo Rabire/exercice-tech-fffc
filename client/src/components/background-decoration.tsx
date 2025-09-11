3b189287b5f8cb20cf9fb155d01347e8ba0d7ca6
Rabire <rabireh@outlook.fr>
2025-09-11 11:10:09 +0200

    style(client): change theme colors & backgound

--- client/src/components/background-decoration.tsx
+++ client/src/components/background-decoration.tsx
@@ -6,12 +6,9 @@ const BackgroundDecoration = ({ children }: PropsWithChildren) => {
       className="absolute inset-0 z-0"
       style={{
         backgroundImage: `
-      linear-gradient(to right, rgba(229,231,235,0.3) 1px, transparent 1px),
-      linear-gradient(to bottom, rgba(229,231,235,0.3) 1px, transparent 1px),
-      radial-gradient(circle 500px at 20% 100%, rgba(139,92,246,0.2), transparent),
-      radial-gradient(circle 500px at 100% 80%, rgba(59,130,246,0.2), transparent)
-      `,
-        backgroundSize: "48px 48px, 48px 48px, 100% 100%, 100% 100%",
+          radial-gradient(125% 125% at 50% 30%, #ffffff 40%, #1b5e20 100%)
+        `,
+        backgroundSize: "100% 100%",
       }}
     >
       {children}
